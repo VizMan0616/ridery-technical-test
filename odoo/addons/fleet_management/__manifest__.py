a77bbe2d59284c8c4ce2e82aa62f75bafa2941c3
José Vizcaya <josevizcaya0616@proton.me>
2025-02-27 21:01:30 -0400

    [ADD] Base module for fleet management

---
+++ odoo/addons/fleet_management/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Gestión de Vehículos",
+    "summary": """
+        Permite la gestión de vehículos y asociación a contactos.
+    """,
+    "author": "José Vizcaya",
+    "category": "Contact/Fleet",
+    "version": "16.0.1.0.0",
+    "depends": ["contacts"],
+    "data": [
+        # 'security/ir.model.access.csv',
+    ],
+}
